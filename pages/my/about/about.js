ebe03474a4652a2a3f7d88b7ce69ed0a48601183
Cloud <cloudswift@qq.com>
2018-08-25 02:23:56 +0800

    add some pages and tab

---
+++ pages/my/about/about.js
@@ -0,0 +1,87 @@
+// pages/my/about/about.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    projectAddress: 'https://github.com/thePixv/SteamCN-Mini-Program',
+    email: 'hi@pixv.me',
+    steam: 's.team/p/cdcc-dnbw',
+    steamcn: "https://steamcn.com/suid-283957"
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function(options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function() {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function() {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function() {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function() {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function() {
+
+  },
+
+  /**
+   * 复制
+   */
+  copy(e) {
+    let dataset = (e.currentTarget || {}).dataset || {}
+    let title = dataset.title || ''
+    let content = dataset.content || ''
+    wx.setClipboardData({
+      data: content,
+      success() {
+        wx.showToast({
+          title: `已复制${title}`,
+          duration: 2000,
+        })
+      },
+    })
+  }
+})
