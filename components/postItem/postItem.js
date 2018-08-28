03afad234e6637a25f91c275b3dbc9068da704e9
Cloud <9652227+xPixv@users.noreply.github.com>
2018-08-29 03:43:55 +0800

    add static thread list to homepage

--- components/postItem/postItem.js
+++ components/postItem/postItem.js
@@ -1,10 +1,14 @@
 // components/postItem.js
+let utils = require('../../utils/utils.js')
 Component({
   /**
    * 组件的属性列表
    */
   properties: {
-
+    list: {
+      type: Array,
+      value: []
+    }
   },
 
   /**
@@ -18,6 +22,14 @@ Component({
    * 组件的方法列表
    */
   methods: {
-
+    toPostDetail(e) {
+      utils.toPostDetail(e)
+    },
+    toPersonal(e) {
+      let item = e.currentTarget.dataset.item
+      wx.navigateTo({
+        url: `/pages/personal/personal?thirduid=${item.user.objectId}`,
+      })
+    },
   }
 })
