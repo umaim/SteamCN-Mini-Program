acb218e153b7b817f4a4b885e6b679f733a151fd
Cloud <cloudswift@qq.com>
2018-08-31 06:12:16 +0800

    add history feature

--- components/postItem/postItem.js
+++ components/postItem/postItem.js
@@ -23,6 +23,29 @@ Component({
   methods: {
     toThreadDetail(e) {
       let item = e.currentTarget.dataset.item;
+
+      wx.getStorage({
+        key: 'history',
+        success: function(res) {
+          let history = res.data;
+          console.log(history);
+          console.log(typeof(history));
+          history.push(item);
+          wx.setStorage({
+            key: 'history',
+            data: history,
+          });
+        },
+        fail: function(res) {
+          let history = [];
+          history.push(item);
+          wx.setStorage({
+            key: 'history',
+            data: history,
+          });
+        }
+      })
+
       wx.navigateTo({
         url: `/pages/thread/thread?url=${item.url}&title=${item.title}&username=${item.user.username || ''}&replied=${item.stats.replied}&viewed=${item.stats.viewed}`
       });
--- pages/my/history/history.js
+++ pages/my/history/history.js
@@ -5,55 +5,62 @@ Page({
    * 页面的初始数据
    */
   data: {
-  
+    threadline: []
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-  
+  onLoad: function(options) {
+    wx.getStorage({
+      key: 'history',
+      success: (res) => {
+        this.setData({
+          threadline: res.data
+        });
+      },
+      fail: (res) => {
+        wx.showToast({
+          title: '先刷刷论坛吧 XD',
+          icon: 'none',
+          duration: 3500
+        });
+      }
+    });
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady: function () {
-  
+  onReady: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
-  onShow: function () {
-  
+  onShow: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide: function () {
-  
+  onHide: function() {
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload: function () {
-  
+  onUnload: function() {
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh: function () {
-  
-  },
+  onPullDownRefresh: function() {
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-  
   }
 })
