7c1e39cb5d1d90c8eb0580d640d326e57c5d69a5
Cloud <cloudswift@qq.com>
2018-08-30 08:47:15 +0800

    add pulldownfresh event

--- pages/index/index.js
+++ pages/index/index.js
@@ -55,7 +55,7 @@ Page({
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function() {
-
+    this.requestHome();
   },
 
   /**
@@ -109,6 +109,7 @@ Page({
             threadline: threadline
           })
           wx.hideLoading();
+          wx.stopPullDownRefresh();
         }
       },
       fail: (res) => {
