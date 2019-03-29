bc0decfd7865494012134daf733cd460340d8cdf
Cloud <cloudswift@qq.com>
2019-03-30 03:26:03 +0800

    feat: complete Hot tab page, add pulldown refresh
    - Complete Hot tab page
    - Add New tab page and Hot tab page pulldown refresh event

--- src/pages/hot/hot.tsx
+++ src/pages/hot/hot.tsx
@@ -1,12 +1,21 @@
 import { ComponentClass } from 'react'
+import { connect } from '@tarojs/redux';
 import Taro, { Component, Config } from '@tarojs/taro'
-import { Text } from '@tarojs/components'
+import { View } from '@tarojs/components'
+
+import ThreadCard from '../../components/ThreadCard/threadCard'
+import { IThreadMeta } from '../../interfaces/thread'
+import { fetchHome } from '../../actions/home'
 
 import './hot.scss'
 
-type PageStateProps = {}
+type PageStateProps = {
+  hotThreadList: IThreadMeta[]
+}
 
-type PageDispatchProps = {}
+type PageDispatchProps = {
+  fetchHome: () => void
+}
 
 type PageOwnProps = {}
 
@@ -17,25 +26,42 @@ type IProps = PageStateProps & PageDispatchProps & PageOwnProps
 interface Hot {
   props: IProps;
 }
-
+@connect(({ home }) => ({
+  hotThreadList: home.hotThreadList
+}), (dispatch) => ({
+  fetchHome() {
+    dispatch(fetchHome())
+  }
+}))
 class Hot extends Component {
   config: Config = {
-    navigationBarTitleText: '热门主题'
+    navigationBarTitleText: '热门主题',
+    enablePullDownRefresh: true
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps)
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  render () {
-    return(
-      <Text>Hot</Text>
+  onPullDownRefresh() {
+    this.props.fetchHome()
+  }
+
+  render() {
+    const { hotThreadList } = this.props
+    const threadCards = hotThreadList.map(item => {
+      return <ThreadCard threadMeta={item} key={item.tid}></ThreadCard>
+    })
+    return (
+      <View className='thread-list'>
+        {threadCards}
+      </View>
     )
   }
 }
