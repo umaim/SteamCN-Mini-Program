fd271c82c0555f1e67532e06dd7c27fa5fce4f9a
Cloud <9652227+xPixv@users.noreply.github.com>
2019-03-31 07:33:49 +0800

    feat: add account page UI
    - Add account page UI, have NOT add any event

--- src/pages/account/account.tsx
+++ src/pages/account/account.tsx
@@ -1,8 +1,11 @@
 import { ComponentClass } from 'react'
 import Taro, { Component, Config } from '@tarojs/taro'
-import { Text } from '@tarojs/components'
+import { Text, View, Image } from '@tarojs/components'
+
+import emptyAvatar from '../../assets/images/empty_avatar_user.png'
 
 import './account.scss'
+import { AtList, AtListItem } from 'taro-ui';
 
 type PageStateProps = {}
 
@@ -23,19 +26,64 @@ class Account extends Component {
     navigationBarTitleText: '我的'
   }
 
-  componentWillReceiveProps (nextProps) {
+  componentWillReceiveProps(nextProps) {
     console.log(this.props, nextProps)
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() { }
 
-  componentDidShow () { }
+  componentDidShow() { }
 
-  componentDidHide () { }
+  componentDidHide() { }
 
-  render () {
-    return(
-      <Text>Account</Text>
+  render() {
+    return (
+      <View className='wrapper'>
+        <View className='profile'>
+
+          <View className='info'>
+            <Image className='avatar' src={emptyAvatar}></Image>
+            <View className='text'>
+              <View className='name'>{'' || '登录'}</View>
+              <View>一直未登录你怎么变强？</View>
+            </View>
+          </View>
+
+          <View className='more at-icon at-icon-chevron-right'></View>
+        </View>
+
+        <View className='forum-area'>
+          <AtList>
+            <AtListItem
+              title='消息中心'
+              iconInfo={{ value: 'bell', color: '#ABB4BF' }}
+            />
+            <AtListItem
+              title='我的收藏'
+              extraText='0个'
+              iconInfo={{ value: 'star-2', color: '#ABB4BF' }}
+            />
+            <AtListItem
+              title='浏览历史'
+              extraText='0篇'
+              iconInfo={{ value: 'clock', color: '#ABB4BF' }}
+            />
+          </AtList>
+        </View>
+
+        <View className='program-area'>
+          <AtList>
+            <AtListItem
+              title='设置'
+              iconInfo={{ value: 'settings', color: '#ABB4BF' }}
+            />
+            <AtListItem
+              title='关于'
+              iconInfo={{ value: 'lightning-bolt', color: '#ABB4BF' }}
+            />
+          </AtList>
+        </View>
+      </View>
     )
   }
 }
