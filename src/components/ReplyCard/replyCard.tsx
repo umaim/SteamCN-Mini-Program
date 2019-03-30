3078fc6467df82d7d5e1bfd9d3137b87f9604dcf
Cloud <9652227+xPixv@users.noreply.github.com>
2019-03-31 00:04:35 +0800

    feat: add ReplyCard component
    - Add ReplyCard component, it 's used in thread detail

---
+++ src/components/ReplyCard/replyCard.tsx
@@ -0,0 +1,67 @@
+import { ComponentClass } from 'react';
+import Taro, { Component } from '@tarojs/taro'
+import { View, Image, Text } from '@tarojs/components';
+import { AtAvatar, AtDivider } from 'taro-ui'
+
+import WxparseRichText from '../WxParseRichText/wxParseRichText'
+
+import './replyCard.scss'
+
+type PageStateProps = {}
+
+type PageDispatchProps = {}
+
+type PageOwnProps = {
+  reply: {
+    user: {
+      username: string;
+      uid: number;
+      avatar: string;
+    };
+    content: string;
+    time: string;
+    floor: number;
+  }
+}
+
+type PageState = {}
+
+type IProps = PageStateProps & PageDispatchProps & PageOwnProps
+
+interface ReplyCard {
+  props: IProps;
+}
+
+class ReplyCard extends Component {
+  static defaultProps = {
+    user: {
+      username: '',
+      uid: 0,
+      avatar: ''
+    },
+    content: '',
+    time: '',
+    floor: 0
+  }
+
+  render() {
+    return (
+      <View className='reply'>
+        <View className='user'>
+          <AtAvatar circle image={this.props.reply.user.avatar} size='small'></AtAvatar>
+          <View className='info'>
+            <Text className='name'>{this.props.reply.user.username}</Text>
+            <Text className='time'>{this.props.reply.time}</Text>
+          </View>
+          <Text className='floor'>{this.props.reply.floor}</Text>
+        </View>
+        <View className='content'>
+          <WxparseRichText html={this.props.reply.content}></WxparseRichText>
+        </View>
+      </View>
+
+    )
+  }
+}
+
+export default ReplyCard as ComponentClass<PageOwnProps, PageState>
