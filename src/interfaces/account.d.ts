4e12ddd5ee19268837040688dd02e0585d117319
Cloud <9652227+xPixv@users.noreply.github.com>
2019-04-15 04:29:04 +0800

    feat: add login page
    - Add Login page
    - Add IAccount interface
    - Show user info in Account page

---
+++ src/interfaces/account.d.ts
@@ -0,0 +1,10 @@
+export interface IAccount {
+  uid: number,
+  username: string,
+  email: string,
+  avatar: string,
+  groupid: number,
+  createdAt: string,
+  UpdatedAt: string,
+  accessToken: string
+}
