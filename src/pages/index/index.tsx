5bf58f320bfec6a1e5f572f8b0eaee6509c7c26c
Cloud <9652227+xPixv@users.noreply.github.com>
2019-04-16 02:55:42 +0800

    fix: rename IHotThreadItemRespond interface
    - Rename again, :P

--- src/pages/index/index.tsx
+++ src/pages/index/index.tsx
@@ -5,7 +5,7 @@ import { AtMessage } from 'taro-ui'
 
 import ThreadCard from '../../components/ThreadCard/threadCard'
 import { IThreadMeta } from '../../interfaces/thread'
-import { IHotThreadItem } from '../../interfaces/respond'
+import { IHotThreadItemRespond } from '../../interfaces/respond'
 
 import './index.scss'
 
@@ -81,7 +81,7 @@ class Index extends Component {
     }).then(res => {
       if (res.statusCode === 200) {
         console.log(res.data)
-        const itemlist = res.data.itemlist as Array<IHotThreadItem>
+        const itemlist = res.data.itemlist as Array<IHotThreadItemRespond>
         let thraedList = Array<IThreadMeta>()
         itemlist.forEach(item => {
           const title = item.title
