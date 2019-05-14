7b5229562b14aa98670b5edaa69d0d25f6340614
Cloud <cloudswift@qq.com>
2019-05-15 04:03:01 +0800

    feat: enhance thread content
    - Remove Steam Widget
    - Remove spoiler, show content directly
    - Replace image `src` from `none.gif` to image true url

--- src/utils/cleaner.ts
+++ src/utils/cleaner.ts
@@ -1,8 +1,12 @@
 export const contentCleaner = (html: string) => {
-  return html.replace(/[\r\n]/g, '') //去掉回车换行
-    .replace(/(<br \/>){2,}/g, '<br/><br/>') //去多余换行
+  return html
+    .replace(/[\r\n]/g, '') //去掉回车
+    .replace(/<iframe src="https:\/\/store\.steampowered.com\/widget\/\d+\/" style="border:none;height:190px;width:100%;max-width:646px;"><\/iframe>/g, '') //去除 Steam Widget 空白
+    .replace(/(<br\s?\/>){2,}/g, '<br/><br/>') //去多余换行
     .replace(/src="forum\.php/g, 'src="https://steamcn.com/forum.php') //相对地址添加域名
     .replace(/src="static/g, 'src="https://steamcn.com/static')
     .replace(/href="forum\.php/g, 'href="https://steamcn.com/forum.php')
     .replace(/font size="(7|6|5)"/g, 'font size="4"') // 最大字号为 4
+    .replace(/"https:\/\/steamcn\.com\/static\/image\/common\/none\.gif" zoomfile=/g, '') //替换默认 src 中的 none 图片
+    .replace(/<div class='showhide'><p>隐藏内容，<a href='javascript:;' onClick="var s=this\.parentNode\.parentNode\.getElementsByClassName\('spoiler'\)\[0\];'none'==s\.style\.display\?\(s\.style\.display='block',this\.innerHTML='点击隐藏'\) : \(s\.style\.display='none',this\.innerHTML='点击显示'\);">点击显示<\/a><\/p><div style='display: none;' class='spoiler'>/g, '<div class=\'showhide\'><div class=\'spoiler\'>') //去除 spoiler 隐藏代码，直接显示隐藏内容
 }
