9ed6923e2f8ad46c4bc2272cee861c4f8743c8b0
Cloud <9652227+xPixv@users.noreply.github.com>
2019-05-15 21:36:18 +0800

    feat: add missing sections and change  description
    - Add missing secitons' metadata
    - Add missing sections' icons

--- src/pages/section/section.tsx
+++ src/pages/section/section.tsx
@@ -29,146 +29,151 @@ class Section extends Component {
     forum: [ //平台周边
       {
         title: '热点聚焦',
-        desc: '业界热点讨论',
+        desc: '周边热门话题讨论',
         fid: 161
       },
       {
         title: '平台研讨',
-        desc: '解决平台技术故障和账号问题',
+        desc: '进阶的技术性交流',
         fid: 127
       },
       {
         title: '华语汉化',
-        desc: '游戏汉化讨论与分享汉化资源',
+        desc: '适用于正版游戏的汉化',
         fid: 257
       },
       {
         title: '成就指南',
-        desc: '研讨游戏成就系统',
+        desc: '游戏成就和达成攻略',
         fid: 235
       },
       {
         title: '游戏互鉴',
-        desc: '发表原创游戏评测',
+        desc: '发布原创游戏评测',
         fid: 129
       },
       {
         title: '福利放送',
-        desc: '免费喜 +1 的机会',
+        desc: '免费领取游戏的机会',
         fid: 319
       },
       {
         title: '购物心得',
-        desc: '交流购物经验以及相关话题',
+        desc: '购物经验和优惠信息',
         fid: 234
       },
       {
         title: '慈善包',
-        desc: '讨论各类游戏包组合 ×禁止发布交易的主题帖',
+        desc: '物美价廉的游戏集合',
         fid: 271
       },
-      /* 需要登录，暂不开放 {
+      {
         title: '交易中心',
-        desc: '',
+        desc: '游戏、卡片、道具、市场',
         fid: 201
       },
       {
         title: '分享互赠',
-        desc: '论坛会员间互赠/换游戏 ×禁止交易',
+        desc: '送出余裕的游戏或道具',
         fid: 254
       },
       {
         title: '资源载点',
-        desc: '下载平台周边资源',
+        desc: '备份、美化、媒体等资源',
         fid: 189
-      }*/
+      }
     ],
     problem: [ //问题互助
-      /*{
+      {
         title: '技术问题',
-        desc: '程序、网络异常 | 账号帮助 | 技术问题',
+        desc: '异常故障和账户帮助',
         fid: 301
-      },*/
+      },
       {
         title: '购物问题',
-        desc: '跨区事宜 | 购物帮助 | 慈善包、促销询问',
+        desc: '跨区、支付、慈善包等问题',
         fid: 302
       },
       {
         title: '社区问题',
-        desc: '论坛积分、账户、制度问题 | 活动咨询',
+        desc: '积分、账号和制度的疑问',
         fid: 304
       },
       {
         title: '资源 / 汉化问题',
-        desc: '汉化、资源寻求',
+        desc: '寻求资源／汉化相关问题',
         fid: 318
       },
       {
         title: '游戏 / 成就问题',
-        desc: '游戏内容、成就问题',
+        desc: '针对某一游戏的特定问题',
         fid: 303
       },
       {
-        title: '福利 / 魔法 / 其它',
-        desc: '其它问题',
+        title: '福利 / 软硬 / 其它',
+        desc: '福利，软硬网络或其他问题',
         fid: 322
+      },
+      {
+        title: '魔法学园',
+        desc: '呼啦呼，雪尼拉，噜巴拉',
+        fid: 311
       }
     ],
     discussion: [ //游戏讨论
       {
         title: '综合讨论',
-        desc: '游戏综合讨论区',
+        desc: '一般游戏交流区',
         fid: 251
       },
       {
         title: '刀塔',
-        desc: 'Dota 2 专区',
+        desc: 'DOTA',
         fid: 305
       },
       {
         title: '全球攻势',
-        desc: 'Counter-Strike: Global Offensive 专区',
+        desc: 'Global Offensive',
         fid: 299
       },
       {
         title: '生存类游戏',
-        desc: '生存类游戏专区',
+        desc: 'Survival 类游戏',
         fid: 291
       },
       {
         title: '侠盗猎车手',
-        desc: 'Grand Theft Auto 专区',
+        desc: 'Grand Theft Auto',
         fid: 312
       },
       {
         title: '威乐',
-        desc: 'Valve 专区',
+        desc: 'Valve 旗下游戏讨论',
         fid: 244
       },
       {
         title: '艺电',
-        desc: 'Electronic Arts 旗下游戏讨论区',
+        desc: 'EA 旗下游戏讨论',
         fid: 246
       },
       {
         title: '育碧',
-        desc: 'Ubisoft 旗下游戏讨论区',
+        desc: 'Ubisoft 旗下游戏讨论',
         fid: 245
       },
       {
         title: '动视暴雪',
-        desc: 'Activision Blizzard 旗下游戏讨论区',
+        desc: 'Activision Blizzard 游戏',
         fid: 248
       }
     ],
     peripheral: [ //论坛周边
       {
         title: '谈天说地',
-        desc: '水区 | 收藏 | 理财 | 健身 | 影音',
+        desc: '论坛指定水区',
         fid: 148
       },
-      /*{
+      {
         title: '旅游摄影',
         desc: '摄影作品 | 旅游图文',
         fid: 259
@@ -182,7 +187,7 @@ class Section extends Component {
         title: '软硬兼施',
         desc: '软件硬件 | 苹果世界 | 安卓天地',
         fid: 200
-      },*/
+      },
       {
         title: '开箱晒物',
         desc: '开箱分享',
@@ -192,64 +197,74 @@ class Section extends Component {
     platform: [ //友商平台
       {
         title: 'Origin',
-        desc: '美商艺电 [EA Origin] - 多款顶级在线游戏的独家发行平台',
+        desc: '艺电旗下游戏平台',
         fid: 232
       },
       {
         title: 'Uplay',
-        desc: '法国育碧 [UBI Uplay] - 含手机端的游戏平台',
+        desc: '育碧旗下游戏平台',
         fid: 274
       },
       {
-        title: '杉果',
-        desc: '杉果 [Sonkwo] - 国内正版游戏分发平台',
-        fid: 316
-      },
-      {
         title: 'GOG',
-        desc: '波兰 [GOG] - 蠢驴良心 DRM-FREE 游戏平台',
+        desc: '无 DRM 机制的自由平台',
         fid: 276
       },
       {
+        title: '杉果',
+        desc: '中电博亚旗下代理平台',
+        fid: 316
+      },
+      {
+        title: 'Epic Games',
+        desc: '虚幻引擎开发商的新推平台',
+        fid: 335
+      },
+      {
+        title: '其它平台',
+        desc: '其他销售网站旗下平台',
+        fid: 277
+      },
+      {
         title: 'Windows 商店',
-        desc: '微软 [Microsoft Store] - Windows 原生游戏及应用分发商店',
+        desc: '微软游戏与软件平台',
         fid: 326
       },
       {
-        title: '方块游戏平台',
-        desc: '方块 [Cubic] - 国内正版游戏分发平台',
+        title: '方块',
+        desc: '方块游戏平台',
         fid: 332
       },
       {
-        title: '腾讯 WeGame',
-        desc: '腾讯 [WeGame] - 原 TGP 平台',
+        title: 'WeGame',
+        desc: '腾讯游戏平台',
         fid: 325
       },
       {
         title: '主机平台',
-        desc: '掌机及家用主机 [Console] - 任天堂 NS、XNS | 索尼 PlayStation | 微软 Xbox 等',
+        desc: 'PS 与 XBOX 等主机',
         fid: 275
       },
       {
         title: '移动平台',
-        desc: '智能终端 [iOS | Android] - Apple Store | Google Play | TapTap 等',
+        desc: '安卓与 iOS 手机游戏',
         fid: 328
-      },
-      {
-        title: '其它平台',
-        desc: '其它游戏分发平台',
-        fid: 277
       }
     ],
     official: [ //社区服务
       {
         title: '公告发布',
-        desc: '社区公告及论坛动态',
+        desc: '社区告示和动态',
         fid: 140
       },
       {
+        title: '投诉反馈',
+        desc: '意见、建议和投诉',
+        fid: 197
+      },
+      {
         title: '社区活动',
-        desc: '社区活动及节日活动',
+        desc: '社区与节日活动',
         fid: 238
       }
     ]
