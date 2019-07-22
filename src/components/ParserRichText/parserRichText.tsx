4d538856766e7350e4ce4dcb0e4013ebf993e415
Cloud <9652227+xPixv@users.noreply.github.com>
2019-07-22 22:00:05 +0800

    refactor: enhance ParserRichText component
    - Add JSDoc commnent
    - Add props description

--- src/components/ParserRichText/parserRichText.tsx
+++ src/components/ParserRichText/parserRichText.tsx
@@ -1,12 +1,75 @@
 import Taro from '@tarojs/taro';
-import { View } from '@tarojs/components';
 
 import './parserRichText.scss';
 
 interface Props {
-  html: string;
+  /**
+   * 富文本数据，可以是 HTML 字符串、节点对象、节点数组
+   *
+   * @default []
+   */
+  html: string | object | object[];
+  /**
+   * 是否允许链接受到点击时自动复制链接（仅限http(s)开头的网络链接）
+   *
+   * @default true
+   */
+  autocopy?: boolean;
+  /**
+   * 是否允许播放视频时自动暂停其他视频
+   *
+   * @default true
+   */
+  autopause?: boolean;
+  /**
+   * 是否允许长按复制内容
+   *
+   * @default false
+   */
+  selectable?: boolean;
+  /**
+   * 标签的默认样式
+   *
+   * @default {}
+   */
+  tagStyle?: object;
+  /**
+   * 图片显示模式
+   *
+   * @default 'default'
+   */
+  imgMode?:
+    | 'default'
+    | 'widthFix'
+    | 'scaleToFill'
+    | 'aspectFit'
+    | 'aspectFill'
+    | 'top'
+    | 'bottom'
+    | 'center'
+    | 'left'
+    | 'right'
+    | 'top left'
+    | 'top right'
+    | 'bottom left'
+    | 'bottom right';
+  /**
+   * 是否使用渐显动画
+   *
+   * @default false
+   */
+  showWithAnimation?: boolean;
+  /**
+   * 渐显动画持续时间
+   *
+   * @default 400
+   */
+  animationDuration?: number;
 }
 
+/**
+ * ParserRichText 富文本组件
+ */
 class ParserRichText extends Taro.Component<Props, {}> {
   public config: Taro.Config = {
     usingComponents: {
@@ -15,15 +78,38 @@ class ParserRichText extends Taro.Component<Props, {}> {
   };
 
   public static defaultProps = {
-    html: ''
+    html: [],
+    autocopy: true,
+    autopause: true,
+    showWithAnimation: false,
+    animationDuration: 400,
+    selectable: false,
+    tagStyle: {},
+    imgMode: 'default'
   };
 
   public render(): JSX.Element {
-    const { html } = this.props;
+    const {
+      html,
+      autocopy,
+      autopause,
+      selectable,
+      tagStyle,
+      imgMode,
+      showWithAnimation,
+      animationDuration
+    } = this.props;
     return (
-      <View>
-        <parser html={html} selectable></parser>
-      </View>
+      <parser
+        html={html}
+        autocopy={autocopy}
+        autopause={autopause}
+        selectable={selectable}
+        tag-style={tagStyle}
+        img-mode={imgMode}
+        show-with-animation={showWithAnimation}
+        animation-duration={animationDuration}
+      ></parser>
     );
   }
 }
