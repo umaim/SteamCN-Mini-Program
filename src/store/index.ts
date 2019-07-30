228c4ba90277fc7033a6049f56a551c7e537a55a
Cloud <9652227+xPixv@users.noreply.github.com>
2019-07-30 23:50:53 +0800

    refactor: lint files

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,34 +1,26 @@
-import { createStore, applyMiddleware, compose, AnyAction, Store } from 'redux'
-import thunkMiddleware from 'redux-thunk'
-import { IAccount } from 'src/interfaces/account';
-import rootReducer from '../reducers'
+import { createStore, applyMiddleware, compose, AnyAction, Store } from 'redux';
+import thunkMiddleware from 'redux-thunk';
+import rootReducer from '../reducers';
 
 const composeEnhancers =
-  typeof window === 'object' &&
-  window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ?
-    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
-      // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
-    }) : compose
+  typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
+    ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({
+        // Specify extension’s options like name, actionsBlacklist, actionsCreators, serialize...
+      })
+    : compose;
 
-const middlewares = [
-  thunkMiddleware
-]
+const middlewares = [thunkMiddleware];
 
 if (process.env.NODE_ENV === 'development') {
-  middlewares.push(require('redux-logger').createLogger())
+  middlewares.push(require('redux-logger').createLogger());
 }
 
 const enhancer = composeEnhancers(
-  applyMiddleware(...middlewares),
+  applyMiddleware(...middlewares)
   // other store enhancers if any
-)
+);
 
-export default function configStore (): Store<{
-  account: {
-    auth: boolean;
-    account: IAccount;
-  };
-}, AnyAction> {
-  const store = createStore(rootReducer, enhancer)
-  return store
+export default function configStore() {
+  const store = createStore(rootReducer, enhancer);
+  return store;
 }
